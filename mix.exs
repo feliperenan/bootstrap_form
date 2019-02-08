5700170ed56e50f4de3a1cba35b30a14bd3a3fd8
Felipe Renan <felipe.renan@plataformatec.com.br>
2019-02-08 11:01:23 -0200

    Add ex_doc lib

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule BootstrapForm.MixProject do
 
   defp deps do
     [
-      {:phoenix_html, ">= 2.13.1"}
+      {:phoenix_html, ">= 2.13.1"},
+      {:ex_doc, "~> 0.18", only: :dev}
     ]
   end
 
